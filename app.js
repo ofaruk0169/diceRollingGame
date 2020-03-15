d9ae366b362313c4b3a686a3f58ecd7f987afb67
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-03-15 10:40:42 +0000

    dice now rolls

--- app.js
+++ app.js
@@ -14,15 +14,29 @@ var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 //need random number for the dice, use Math Random fucntion to roll the dice
 //Multiply by 6 for a number between 0 and 5, use floor for full numbers. Also add 1 for dice mapping
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent =  dice;
+
+// document.querySelector('#current-' + activePlayer).textContent =  dice;
 // document.querySelector('#current-' + activePlayer).innerHTML =  '<em>' + dice + '</em>'
 
 document.querySelector('.dice').style.display = 'none';
+
+
+//select button, how to use Event Listeners. First argument is the event and the second event is the function you wnat the event to call
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1 Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. display the dice results
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+
+    //3 Update the round score IF the rolled number was NOT a 1
+});
